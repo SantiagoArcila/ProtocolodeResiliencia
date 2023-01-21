54d82dba0cde750761911dd2ff03eda3db1f02f7
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2023-01-21 17:05:24 -0500

    organizing task

--- tests/node_test.rb
+++ tests/node_test.rb
@@ -6,22 +6,18 @@ class TestNode < Test::Unit::TestCase
   def test_node_creation 
     node = Node.new(100)
     assert_equal(node.balance,100)
-    assert_equal(node.incoming_edge, nil)
-    assert_equal(node.outgoing_edge, nil)
+    assert_equal(node.incoming_edge_from_node, nil)
+    assert_equal(node.outgoing_edge_to_node, nil)
   end
 
   def test_node_modification
     node = Node.new(100)
     assert_equal(node.balance,100)
-    assert_equal(node.incoming_edge, nil)
-    assert_equal(node.outgoing_edge, nil)
-    assert_raise "undefined method" do
-      node.id = 2
-    end
-    node.incoming_edge = Node.new(100)
-    node.outgoing_edge = Node.new(100)
+    
+    node.incoming_edge_from_node = Node.new(100)
+    node.outgoing_edge_to_node = Node.new(200)
     assert_equal(node.balance = 200, 200)
-    assert node.incoming_edge
-    assert node.outgoing_edge
+    assert node.incoming_edge_from_node
+    assert node.outgoing_edge_to_node
   end
 end
