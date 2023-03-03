1c7cdac722ceead7d60b121f06de8ae65d08b3cb
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2023-03-02 22:31:27 -0500

    working on graph creation

--- resilence_protocol/lib/resilence_protocol.ex
+++ resilence_protocol/lib/resilence_protocol.ex
@@ -1,18 +1,16 @@
 defmodule ResilenceProtocol do
   @moduledoc """
-  Documentation for `ResilenceProtocol`.
+  initial effor to simulate `ResilenceProtocol` behaviour.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ResilenceProtocol.hello()
-      :world
-
+  creates a random tree with given the quantity of nodes
   """
-  def hello do
-    :world
+  @spec create_random_tree(integer()) :: :gb_trees
+  def create_random_tree(nodes) do
+    Enum.reduce(1..nodes, :gb_trees.empty(), fn node, tree ->
+      value = :rand.uniform(100)
+      :gb_trees.insert(node, value, tree)
+    end)
   end
 end
--- resilence_protocol/test/resilence_protocol_test.exs
+++ resilence_protocol/test/resilence_protocol_test.exs
@@ -2,7 +2,8 @@ defmodule ResilenceProtocolTest do
   use ExUnit.Case
   doctest ResilenceProtocol
 
-  test "greets the world" do
-    assert ResilenceProtocol.hello() == :world
+  test "creates a random tree given the num of nodes" do
+    tree = ResilenceProtocol.create_random_tree(10)
+    assert :gb_trees.size(tree) == 10
   end
 end
