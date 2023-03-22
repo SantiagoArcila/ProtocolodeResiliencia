a91a8507ee9b525bb7a1584a761adef799f547e4
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2023-03-22 04:59:43 +0000

    adding values to the nodes

--- resilence_protocol/lib/resilence_protocol.ex
+++ resilence_protocol/lib/resilence_protocol.ex
@@ -2,4 +2,39 @@ defmodule ResilenceProtocol do
   @moduledoc """
   initial effor to simulate `ResilenceProtocol` simulation.
   """
+
+  # hold any necesary info that the graph (nodes and edges) will need
+  @node_table Module.concat(__MODULE__, "node_properties")
+  @edge_table Module.concat(__MODULE__, "edge_properties")
+
+  @spec initialice_protocol() :: Graph.t()
+  def initialice_protocol() do
+    create_ets_tables()
+    Graph.new()
+  end
+
+  @doc """
+  creates the node if does not exists
+  """
+  # @spec set_balance(number()) :: :ok
+  def set_balance(graph, node, value) do
+    :ets.insert(@node_table, {:balance, value})
+    Graph.add_vertex(graph, node)
+  end
+
+  # @spec get_balance(number()) :: :ok
+  def get_balance() do
+    :ets.lookup(@node_table, :balance)
+  end
+
+  def print_tables_info() do
+    :ets.i(@node_table)
+    :ets.i(@edge_table)
+  end
+
+  @spec create_ets_tables() :: :ok
+  defp create_ets_tables() do
+    :ets.new(@node_table, [:named_table])
+    :ets.new(@edge_table, [:named_table])
+  end
 end
--- resilence_protocol/test/resilence_protocol_test.exs
+++ resilence_protocol/test/resilence_protocol_test.exs
@@ -1,3 +1,7 @@
 defmodule ResilenceProtocolTest do
   use ExUnit.Case
+
+  test "creates a graph with balance for each node" do
+    flunk("to implement")
+  end
 end
